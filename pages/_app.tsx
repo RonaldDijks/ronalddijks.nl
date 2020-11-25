7e4b946602c52fdb154865e7927929e624299bfb
RonaldDijks <ronalddijks@protonmail.com>
2020-11-25 01:59:06 +0100

    add chakra

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import type { AppProps } from "next/app";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
+};
+
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
+import { Box, Heading } from "@chakra-ui/react";
+import React from "react";
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <Box
+      display="flex"
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Heading>Welcome to Next.js!</Heading>
+    </Box>
+  );
 }
 
 export default HomePage;
