3c21685e2f6c80ff1ead311271e3d19ccbc61ac3
Ronald Dijks <ronald@rdrd.io>
2022-05-01 14:03:37 +0200

    add eslint config

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,21 +9,23 @@ export interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = (props) => {
-  const {
-    lastListened: song
-  } = props;
+  const { lastListened: song } = props;
 
-  return <header className="flex items-center justify-between p-4">
-    <section className="uppercase font-medium text-xs">
-      <span>
-        <time className="mr-2">Last listened to</time>
-        <a href={song.link} className="underline">{song.artist} - {song.song}</a>
-      </span>
-    </section>
-    <nav>
-      <button>☀️</button>
-    </nav>
-  </header>
-}
+  return (
+    <header className="flex items-center justify-between p-4">
+      <section className="uppercase font-medium text-xs">
+        <span>
+          <time className="mr-2">Last listened to</time>
+          <a href={song.link} className="underline">
+            {song.artist} - {song.song}
+          </a>
+        </span>
+      </section>
+      <nav>
+        <button>☀️</button>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import Image from 'next/image'
-import imageSource from '../assets/me.jpg'
-import Header from '../components/Header'
+import React from "react";
+import Image from "next/image";
+import imageSource from "../assets/me.jpg";
+import Header from "../components/Header";
 
 interface LinkProps {
   href: string;
@@ -9,32 +9,59 @@ interface LinkProps {
 }
 
 const Link: React.FC<LinkProps> = ({ children, href }) => {
-  return <a className='underline' href={href}>{children}</a>
-}
+  return (
+    <a className="underline" href={href}>
+      {children}
+    </a>
+  );
+};
 
 const IndexPage: React.FC = () => {
-  return <div>
-    <Header lastListened={{ artist: 'Meshuggah', song: 'Bleed', link: 'https://www.last.fm' }} />
-    <div className='max-w-3xl mx-auto'>
-      <section className='py-40'>
-        <div className="flex">
-          <div className='max-w-xs'>
-            <Image
-              className='rounded-lg'
-              src={imageSource}
-              alt="Headshot of Ronald Dijks"
-            />
+  return (
+    <div>
+      <Header
+        lastListened={{
+          artist: "Meshuggah",
+          song: "Bleed",
+          link: "https://www.last.fm",
+        }}
+      />
+      <div className="max-w-3xl mx-auto">
+        <section className="py-40">
+          <div className="flex">
+            <div className="max-w-xs">
+              <Image
+                className="rounded-lg"
+                src={imageSource}
+                alt="Headshot of Ronald Dijks"
+              />
+            </div>
+            <div className="ml-8">
+              <h1 className="text-2xl font-bold mb-2">Hi, I&apos;m Ronald.</h1>
+              <p className="mb-4">
+                I&apos;m a software developer from Amsterdam. I work at Crisp as
+                a fullstack developer, and I run Essential Audio with two of my
+                friends.
+              </p>
+              <p className="mb-4">
+                You can find me on{" "}
+                <Link href="https://github.com/RonaldDijks">GitHub</Link>,{" "}
+                <Link href="https://www.linkedin.com/in/ronalddijks/">
+                  LinkedIn
+                </Link>{" "}
+                and <Link href="https://twitter.com/Ronald_Dijks">Twitter</Link>
+                .
+              </p>
+              <p className="mb-4">
+                I&apos;m also available for hire as a fullstack freelancer, if
+                you have any fun projects, shoot me a message!
+              </p>
+            </div>
           </div>
-          <div className="ml-8">
-            <h1 className='text-2xl font-bold mb-2'>Hi, I'm Ronald.</h1>
-            <p className='mb-4'>I'm a software developer from Amsterdam. I work at Crisp as a fullstack developer, and I run Essential Audio with two of my friends.</p>
-            <p className='mb-4'>You can find me on <Link href="https://github.com/RonaldDijks">GitHub</Link>, <Link href="https://www.linkedin.com/in/ronalddijks/">LinkedIn</Link> and <Link href="https://twitter.com/Ronald_Dijks">Twitter</Link>.</p>
-            <p className='mb-4'>I'm also available for hire as a fullstack freelancer, if you have any fun projects, shoot me a message!</p>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
